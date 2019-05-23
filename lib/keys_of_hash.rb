e8c0e7d89f531a057e8fea5f067b9c35c196f1ad
Angela Lo <angelawenlo@gmail.com>
2019-05-23 15:57:29 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
+require 'pry'
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
-  end
+    animals = []
+      arguments.each do |places|
+      self.each do |animal, place|
+        if place == places
+          animals << animal
+        end
+      end
+      end
+      animals
+end
 end
