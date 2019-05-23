66d7ee6d91912a0281456d8afb31acf82aff6072
Angela Lo <angelawenlo@gmail.com>
2019-05-23 15:48:03 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,7 +3,10 @@ class Hash
   def keys_of(*arguments)
     # code goes here
     animals = []
+      arguments.each do |country|
       self.each do |k, v|
+        if country == v
+          animals << k
     binding.pry
     end
   end
